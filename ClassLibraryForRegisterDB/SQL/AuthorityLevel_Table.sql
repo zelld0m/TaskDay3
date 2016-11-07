8230b9ee75f4ff042044c011aa437cb3f0499fe0
Aizel Nunag <izelllim@gmail.com>
2016-11-07 11:43:18 +0800

    SQL Query Added

---
+++ ClassLibraryForRegisterDB/SQL/AuthorityLevel_Table.sql
@@ -0,0 +1,19 @@
+
+
+
+use Registration;
+--drop table AuthorityLevel
+CREATE TABLE AuthorityLevel
+(
+A_Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+AuthorityName varchar(255) NOT NULL,
+AccessLevel int NOT NULL
+
+)
+
+
+insert into AuthorityLevel(AuthorityName,AccessLevel)
+values('User_1',1),('User_2',2),('User_3',3),('HalfAdmin',4),('FULL-Admin',5)
+
+
+
---
+++ ClassLibraryForRegisterDB/SQL/Registration_Person_Table.sql
@@ -0,0 +1,28 @@
+
+
+use Registration;
+drop table Persons
+CREATE TABLE Persons
+(
+P_Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+LastName varchar(255) NOT NULL,
+FirstName varchar(255),
+Username varchar(255),
+Password varchar(255),
+Address varchar(255),
+City varchar(255),
+A_ID int NOT NULL,
+
+)
+select * from Persons
+
+
+
+
+
+use Registration;
+
+insert into Persons(LastName,FirstName,Username,Password,Address,City,A_ID)
+values('aizel','nunag','zell','potato','bangkal','makati',5),
+('noel','corales','noel','humanR','shaw','mandaluyong',1),
+('lim','elisa','elsie','kap','nova','qc',4);
