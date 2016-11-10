cc6c618e04e8e3f0418cee7fa9d1dac8cd648e51
Aizel Nunag <izelllim@gmail.com>
2016-11-10 14:00:18 +0800

    Added Clear textbox Feature

--- DummyForm/Form1.cs
+++ DummyForm/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ClassLibraryForRegisterDB;
+//using DummyWcfServiceLibrary1;
 namespace DummyForm
 {
     public partial class Form1 : Form
@@ -29,18 +30,22 @@ namespace DummyForm
         void refresh()
         {
             dataGridView1.DataSource = DAL.ViewDummy();
+            ClearControls();
         }
 
 
         private void Btn_search_Click(object sender, EventArgs e)
         {
             dataGridView1.DataSource=  DAL.SearchDummy(Tb_Search.Text);
+
+            ClearControls();
         }
 
         private void Btn_Add_Click(object sender, EventArgs e)
         {
             DAL.AddDummy(Tb_Add.Text);
             refresh();
+            ClearControls();
         }
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
@@ -51,6 +56,7 @@ namespace DummyForm
         private void button1_Click(object sender, EventArgs e)// refreshBtn
         {
             refresh();
+            ClearControls();
         }
 
         private void Tb_Delete_TextChanged(object sender, EventArgs e)
@@ -62,7 +68,7 @@ namespace DummyForm
         {
             DAL.DeleteDummy(Convert.ToInt32(Tb_Delete.Text));
             refresh();
-
+            ClearControls();
         }
 
         private void Tb_Search_TextChanged(object sender, EventArgs e)
@@ -74,6 +80,16 @@ namespace DummyForm
         {
             DAL.UpdateDummy(Convert.ToInt32(TB_SelectID.Text), Tb_NameChange.Text);
             refresh();
+            ClearControls();
+        }
+
+        private void ClearControls()
+        {
+            Tb_Add.Text = "";
+            Tb_Delete.Text = "";
+            Tb_NameChange.Text = "";
+            Tb_Search.Text = "";
+            TB_SelectID.Text = "";
         }
     }
 }
